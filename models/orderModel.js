89942b03aaf8483ef298da94452fc150e8ca901e
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-05-03 23:57:57 +0300

    Modified order model

--- models/orderModel.js
+++ models/orderModel.js
@@ -51,6 +51,18 @@ const orderSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+orderSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "user",
+    select: "name profileImg email phone",
+  }).populate({
+    path: "cartItems.product",
+    select: "title imageCover ",
+  });
+
+  next();
+});
+
 const Order = mongoose.model("Order", orderSchema);
 
 module.exports = Order;
